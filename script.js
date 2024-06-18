f26c2fc134c0f7b8a9e60b8776e8ccffaaa2b8ea
auth-xyz <authenticover@gmail.com>
2024-06-17 23:58:19 -0400

    i hate you github

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
 
+    // Check local storage for theme preference
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        themeToggle.checked = currentTheme === 'light-theme';
+    }
+
     themeToggle.addEventListener('change', () => {
-        document.body.classList.toggle('light-theme', themeToggle.checked);
+        if (themeToggle.checked) {
+            document.body.classList.remove('dark-theme');
+            document.body.classList.add('light-theme');
+            localStorage.setItem('theme', 'light-theme');
+        } else {
+            document.body.classList.remove('light-theme');
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark-theme');
+        }
     });
 });
+
